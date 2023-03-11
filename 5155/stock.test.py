6f4fa5f3388231e7b3016618c5fac6b526861fe4
Tung <toilatung52@gmail.com>
2023-03-11 11:16:02 +0700

    Update app python for generating data

--- 5155/stock.test.py
+++ 5155/stock.test.py
@@ -5,42 +5,46 @@ import random
 from kafka import KafkaProducer
 
 
-def random_temp_cels():
-    return round(random.uniform(-10, 50), 1)
+def random_timestamp():
+    return round(time.time())
 
-def random_humidity():
+def random_portfolio_id():
+    return round(random.uniform(0, 200))
+
+def random_target_profit():
     return round(random.uniform(0, 100), 1)
 
-def random_wind():
-    return round(random.uniform(0, 10), 1)
+def random_realized_profit_loss():
+    return round(random.uniform(0, 100), 1)
 
-def random_soil():
+def random_unrealized_profit_loss():
     return round(random.uniform(0, 100), 1)
 
 def get_json_data():
     data = {}
 
-    data["temperature"] = random_temp_cels()
-    data["humidity"] = random_humidity()
-    data["wind"] = random_wind()
-    data["soil"] = random_soil()
+    data["Timestamp"] = random_timestamp()
+    data["PortfolioID"] = random_portfolio_id()
+    data["TargetProfit"] = random_target_profit()
+    data["RealizedProfitLoss"] = random_realized_profit_loss()
+    data["UnrealizedProfit(Loss)"] = random_unrealized_profit_loss()
 
-    return json.dumps(data) 
+    return json.dumps(data)
 
 def main():
-    producer = KafkaProducer(
-        bootstrap_servers=['bootstrap.myingress.com:443'],
-        security_protocol='SSL',
-        ssl_cafile='./cert/ca.crt',
-        ssl_certfile='./cert/user.crt',
-        ssl_keyfile='./cert/user.key',
-    )
+    # producer = KafkaProducer(
+    #     bootstrap_servers=['bootstrap.myingress.com:443'],
+    #     security_protocol='SSL',
+    #     ssl_cafile='./cert/ca.crt',
+    #     ssl_certfile='./cert/user.crt',
+    #     ssl_keyfile='./cert/user.key',
+    # )
 
     for _ in range(20000):
         json_data = get_json_data()
-        producer.send('test', bytes(f'{json_data}','UTF-8'))
+        # producer.send('test', bytes(f'{json_data}','UTF-8'))
         print(f"Sensor data is sent: {json_data}")
-        time.sleep(5)
+        time.sleep(2)
 
 
 if __name__ == "__main__":
