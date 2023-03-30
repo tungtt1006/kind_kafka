8818b0b4cd81f17dd01de37f1e0d09ce16976501
Tung <toilatung52@gmail.com>
2023-03-30 23:42:07 +0700

    Test prometheus config

--- 5155/prometheus/deploy.sh
+++ 5155/prometheus/deploy.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
+# https://artifacthub.io/packages/helm/prometheus-community/kube-prometheus-stack/43.3.1
+
 helm upgrade kube-prometheus-stack prometheus-community/kube-prometheus-stack \
     -i \
     --version 44.3.1 \
