7c5fd1657726e798b0c9cb7cd9726586d043018a
zinza <toilatung52@gmail.com>
2023-04-18 15:14:28 +0700

    Scale test

--- 5155/nginx-ingress/deploy.sh
+++ 5155/nginx-ingress/deploy.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/bash
 
-kubectl apply -f nginx-ingress/values.yaml
+# kubectl apply -f nginx-ingress/values.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
